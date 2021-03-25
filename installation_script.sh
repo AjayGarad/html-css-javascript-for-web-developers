6c4a16e2f2ca05064c590f97fb1b2ef6e0b1e00d
Ajay Garad <ajayg@dosepack.com>
2021-03-25 23:37:12 +0530

    update - README.md - package requirement, brower_sync usage

--- installation_script.sh
+++ installation_script.sh
@@ -1,3 +1,4 @@
+# https://github.com/nodesource/distributions/blob/master/README.md#debinstall
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 node --version
